0fbc04dd8ed8c80a73dbb26d85a96309479b4323
unknown <k.jones45@yahoo.com>
2023-05-12 22:54:43 -0400

    registered admin site for Comment model. + started POST endpoit.

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -4,12 +4,14 @@ from .serializers import CommentSerializer
 from .models import Comment
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def comments_list(request):
-    comments = Comment.objects.all()
-    serializer = CommentSerializer(comments, many = True)
+    if request.method == 'GET':
 
-
-    return Response(serializer.data)
-    
+        comments = Comment.objects.all()
+        serializer = CommentSerializer(comments, many = True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        return()
+        
 
