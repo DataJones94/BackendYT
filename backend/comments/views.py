adf56189c7a6ac915c774b2d09ab496cea392530
unknown <k.jones45@yahoo.com>
2023-05-12 20:39:05 -0400

    added commentserializer to api_view GET request in views.py

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,11 +1,15 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import CommentSerializer
+from .models import Comment
+
 
 @api_view(['GET'])
 def comments_list(request):
-    
+    comments = Comment.objects.all()
+    serializer = CommentSerializer(comments, many = True)
 
 
-    return Response('ok')
+    return Response(serializer.data)
     
 
