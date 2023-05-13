280a4b07495e6a15ffcde1f15e1901add663ce3c
unknown <k.jones45@yahoo.com>
2023-05-13 10:12:53 -0400

    completed POST functionality for comment.

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,17 +1,23 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import CommentSerializer
 from .models import Comment
 
 
 @api_view(['GET', 'POST'])
 def comments_list(request):
-    if request.method == 'GET':
 
+    if request.method == 'GET':
         comments = Comment.objects.all()
         serializer = CommentSerializer(comments, many = True)
         return Response(serializer.data)
+    
     elif request.method == 'POST':
-        return()
+        serializer = CommentSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)                                       
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
         
 
